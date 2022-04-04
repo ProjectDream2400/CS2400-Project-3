6675bb518ebe71f97d8e8b42f231146e1fe1f2d9
gbermudez2 <gbermudezahs@gmail.com>
2022-04-03 22:15:10 -0700

    Task 4 complete

--- src/DriverBT.java
+++ src/DriverBT.java
@@ -62,22 +62,43 @@ public class DriverBT
 
 		tree.setTree("A", bTree, cTree);
 		
-      System.out.println("\nGiven Tree:\n");
-      System.out.println("     A      ");
-      System.out.println("   /   \\  ");
-      System.out.println("  B     C  ");
-      System.out.println(" / \\   /  ");
-      System.out.println("D   E  F   ");
-      System.out.println("        \\ ");
-      System.out.println("         G ");
-      System.out.println();
+      	System.out.println("\nGiven Tree:\n");
+      	System.out.println("     A      ");
+      	System.out.println("   /   \\  ");
+      	System.out.println("  B     C  ");
+      	System.out.println(" / \\   /  ");
+      	System.out.println("D   E  F   ");
+      	System.out.println("        \\ ");
+      	System.out.println("         G ");
+      	System.out.println();
 	} // end createTree
 	
 	/**-------------------------------------------------------------------- */
 	/**Task 4: Create a tree case as shown in assignment 3*/
 	public static void createTree2(BinaryTree<String> tree)
 	{ 
+		// Leaves
+		BinaryTree<String> bTree = new BinaryTree<>("B");
+		BinaryTree<String> fTree = new BinaryTree<>("F");
+		BinaryTree<String> gTree = new BinaryTree<>("G");
+		BinaryTree<String> hTree = new BinaryTree<>("H");
 
+		// Subtrees:
+		BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
+		BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
+		BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+
+		tree.setTree("A", bTree, cTree);
+
+		System.out.println("\nGiven Tree:\n");
+      	System.out.println("     A      ");
+      	System.out.println("   /   \\   ");
+      	System.out.println("  B     C   ");
+      	System.out.println("       / \\ ");
+      	System.out.println("      D   E ");
+      	System.out.println("     /   / \\  ");
+      	System.out.println("    F   G   H  ");
+      	System.out.println();
 	} // end createTree2
 
 }  // end DriverBT
